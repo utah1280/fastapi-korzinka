dda019cc1c7d8376e0a4c2683dd767b40bd341f9
utah1280 <ravshan.yaml@gmail.com>
2024-07-04 16:29:07 +0500

    crud: categor read by id

--- fastapi-application/crud/category.py
+++ fastapi-application/crud/category.py
@@ -7,3 +7,8 @@ async def get_all_categories(session: AsyncSession):
     stmt = select(Categories).order_by(Categories.id)
     result = await session.scalars(stmt)
     return result.all()
+
+async def get_category_by_id(id: int, session: AsyncSession):
+    stmt = select(Categories).where(Categories.id == id)
+    result = await session.scalar(stmt)
+    return result
