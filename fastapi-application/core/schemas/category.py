2278838f8be6af6723e5d4cf829216e5af8b4b54
utah1280 <ravshan.yaml@gmail.com>
2024-07-04 15:54:51 +0500

    added response schemas

---
+++ fastapi-application/core/schemas/category.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class CategoryBase(BaseModel):
+    id: int
+    label: str
+    created_at: datetime
+
+class CreateCategory(CategoryBase):
+    success: bool
