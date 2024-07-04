5a6cc03d04b34c22dcf387ff5490959c8978f6b6
utah1280 <ravshan.yaml@gmail.com>
2024-07-04 12:51:07 +0500

    added base model

---
+++ fastapi-application/core/models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped 
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import declared_attr
+from sqlalchemy import DateTime, func
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), nullable=False)
