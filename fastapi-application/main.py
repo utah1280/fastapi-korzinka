969718a5a8030e6383c14c2f4c50520f8dd2673d
utah1280 <ravshan.yaml@gmail.com>
2024-07-05 20:32:45 +0500

    .

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,19 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-
-from api import router as api_router
 from core.config import settings
 from core.models import db_helper
-from core.models import Base
+from api import router as api_router
+from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startup
+    # Sturtup
     yield
     # Shutdown
     await db_helper.dispose()
-    
+
 main_app = FastAPI(
     lifespan=lifespan,
 )
@@ -27,5 +25,5 @@ if __name__ == "__main__":
         app="main:main_app",
         host=settings.run.host,
         port=settings.run.port,
-        reload=True,
+        reload=True
     )
