0d9be9ad59d44d857712aaa572d0e15c6218fe24
utah1280 <ravshan.yaml@gmail.com>
2024-07-06 10:55:12 +0500

    added biderectional relationship

--- fastapi-application/core/models/tables.py
+++ fastapi-application/core/models/tables.py
@@ -1,10 +1,12 @@
 from .base import Base
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
 
 class Categories(Base):
     label: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+    contacts: Mapped["Contacts"] = relationship("Contacts", back_populates="category")
 
 class Contacts(Base):
     name: Mapped[str] = mapped_column(nullable=False)
@@ -12,3 +14,5 @@ class Contacts(Base):
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     address: Mapped[str] = mapped_column(nullable=False)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+
+    category: Mapped[Categories] = relationship("Categories", back_populates="contacts")
