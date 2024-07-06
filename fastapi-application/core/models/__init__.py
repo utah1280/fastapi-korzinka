c2bd5b3dd0be9e44cf71e226219575b508ec2a03
utah1280 <ravshan.yaml@gmail.com>
2024-07-06 15:46:25 +0500

    .

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Base"
     "Contacts"
     "Categories"
+    "Users"
 )
 
 from .db_helper import db_helper
 from.base import Base
+from .tables import Users
 from .tables import Contacts
 from .tables import Categories
