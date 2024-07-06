14262d296681c7a16ae7d78fba0abd8016017c05
utah1280 <ravshan.yaml@gmail.com>
2024-07-06 15:47:37 +0500

    added users schemas

---
+++ fastapi-application/core/schemas/user_schemas.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class User(BaseModel):
+    username: str
+    email: str
